de4f48aa6f07ecd4a133725a5ca81c0b5c192955
Carl Adolfson <cadolfson@outlook.com>
2018-10-22 10:37:57 -0500

    Add family generation

--- db/migrate/20181020193300_create_families.rb
+++ db/migrate/20181020193300_create_families.rb
@@ -1,6 +1,7 @@
 class CreateFamilies < ActiveRecord::Migration[5.2]
   def change
     create_table :families do |t|
+      t.integer :generation
       t.references :house, foreign_key: true
 
       t.timestamps
