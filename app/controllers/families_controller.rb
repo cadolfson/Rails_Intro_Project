ca476e8e52e650610f196786fdff1af647e01df5
Carl Adolfson <cadolfson@outlook.com>
2018-10-23 09:14:06 -0500

    Add family, house and dog controllers

---
+++ app/controllers/families_controller.rb
@@ -0,0 +1,7 @@
+class FamiliesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'dogs/index'
+  get 'dogs/show'
+  get 'dogss/index'
+  get 'dogss/show'
+  get 'houses/index'
+  get 'houses/show'
+  get 'house/index'
+  get 'house/show'
+  get 'families/index'
+  get 'families/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
